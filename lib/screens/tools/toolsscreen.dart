0a0530e6fe922b0847e663fdfdd6c26d895806eb
masterdodo <imdavidarist@gmail.com>
2020-09-24 00:44:18 +0200

    noisemeter tool added - mostly works

--- lib/screens/tools/toolsscreen.dart
+++ lib/screens/tools/toolsscreen.dart
@@ -1,6 +1,8 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:noise_app/app_localizations.dart';
 import 'package:noise_app/components/my_drawer.dart';
+import 'package:noise_meter/noise_meter.dart';
 
 class ToolsScreen extends StatefulWidget {
   @override
@@ -8,7 +10,63 @@ class ToolsScreen extends StatefulWidget {
 }
 
 class _ToolsScreenState extends State<ToolsScreen> {
+  bool _isRecording;
+  StreamSubscription<NoiseReading> _noiseSubscription;
+  NoiseMeter _noiseMeter;
+
+  String maxDb;
+  String meanDb;
+
   Color _bgColor = Colors.white;
+
+  @override
+  void initState() {
+    super.initState();
+    _noiseMeter = new NoiseMeter();
+    _isRecording = false;
+    maxDb = "30.00";
+    meanDb = "30.00";
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  void start() async {
+    try {
+      _noiseSubscription = _noiseMeter.noiseStream.listen(onData);
+    } catch (exception) {
+      print(exception);
+    }
+  }
+
+  void stop() {
+    try {
+      if (_noiseSubscription != null) {
+        _noiseSubscription.cancel();
+        _noiseSubscription = null;
+      }
+      this.setState(() {
+        this._isRecording = false;
+      });
+    } catch (err) {
+      print('stopRecorder error: $err');
+    }
+  }
+
+  void onData(NoiseReading noiseReading) {
+    this.setState(() {
+      if (!this._isRecording) {
+        this._isRecording = true;
+      }
+    });
+    setState(() {
+      this.maxDb = noiseReading.maxDecibel.toStringAsFixed(2);
+      this.meanDb = noiseReading.meanDecibel.toStringAsFixed(2);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,7 +75,51 @@ class _ToolsScreenState extends State<ToolsScreen> {
       appBar: AppBar(
           title: Text(
               AppLocalizations.of(context).translate('menu_tools_string'))),
-      body: null,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(10.0),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(bottom: 15),
+                child: Row(
+                  children: [
+                    Text(
+                      'Noise Meter',
+                      style:
+                          TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Text(
+                    "Average dB",
+                    style:
+                        TextStyle(fontSize: 20, fontWeight: FontWeight.normal),
+                  ),
+                  RaisedButton(
+                    padding: EdgeInsets.all(15),
+                    onPressed: _isRecording ? this.stop : this.start,
+                    color: _isRecording ? Colors.red : Colors.green,
+                    shape: CircleBorder(),
+                    child: _isRecording
+                        ? Icon(Icons.stop)
+                        : Icon(Icons.play_arrow),
+                  ),
+                  Text(this.meanDb,
+                      style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.normal,
+                      )),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
